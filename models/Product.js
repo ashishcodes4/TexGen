de5191980a9017264339bdc0cb681dcf37333015
Ashish Singh <ashishcodes4@gmail.com>
2018-07-26 22:09:10 +0530

    product model

--- models/Product.js
+++ models/Product.js
@@ -10,9 +10,33 @@ const ProductSchema = new Schema({
     {
       name: {
         type: String
-      }
-    }
-  ]
+      },
+      categories: [
+        {
+          name: {
+            type: String
+          },
+          subCategories: [
+            {
+              name: {
+                type: String
+              },
+              topics: [
+                {
+                  name: {
+                    type: String
+                  },
+                  text: {
+                    type: String
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
 });
 
 module.exports = Product = mongoose.model('Product', ProductSchema);
